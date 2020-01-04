8dbdfba82b412675a0b43fe02a9210e82944a35a
Anthony Holder <anthonyh202x@gmail.com>
2020-01-04 21:05:34 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -36,7 +36,16 @@ def naming_system
    sub_category: {
      label: "Phylum",
      sub_category: {
-       label: "Class"
+       label: "Class", 
+       sub_category: {
+         label: "Order",
+         sub_category: {
+           label: "Family"
+           sub_category: {
+             
+            }
+          }
+       } 
      }
    }
  
